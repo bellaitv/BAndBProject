2303ac4aa815a0a75da280ee2de44f84605b7b6e
bellaitv <bellaitv@gmail.com>
2018-06-14 13:38:44 +0200

    create Item

---
+++ BAndBProject/Controllers/ItemsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BAndBProject.Models;
+
+namespace BAndBProject.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items
+        public ActionResult Index()
+        {
+
+            var item = new Item { Name = "TEST ITEM" };
+            return View(item);
+        }
+    }
+}
---
+++ BAndBProject/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAndBProject.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public int Price { get; set; }
+        public int Type { get; set; }
+    }
+}
